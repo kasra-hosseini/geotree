3f9d94e4325209f37984fb1b51ba80dec2906672
kasra-hosseini <khosseini@turing.ac.uk>
2021-05-28 12:32:27 +0100

    refactor: move kdtree_core funcs to tree_core; add a new notebook

--- geotree/tree_core/tree.py
+++ geotree/tree_core/tree.py
@@ -6,23 +6,27 @@ __license__ = "MIT License"
 
 import numpy as np
 from scipy import spatial
+from sklearn.neighbors import BallTree
 from typing import Union
 
 from geotree.utils import convert
 
-class gkdtree:
+class gtree:
     def __init__(self):
         # xyz: base
         # kdt: kd-tree created from base
         # xyz_q: query
         self.xyz = None
-        self.kdt = None
         self.xyz_q = None
+        self.kdt = None
+        self.balltree = None
+        self.earth_radius = 6371000
 
     def add_lonlatdep(self, 
                       lons: Union[float, int, np.ndarray, list], 
                       lats: Union[float, int, np.ndarray, list], 
-                      depths: Union[float, int, np.ndarray, list, None]=None):
+                      depths: Union[float, int, np.ndarray, list, None]=None,
+                      convert2xyz: bool=True):
         """Add lons/lats/depths of the base
 
         Parameters
@@ -31,7 +35,11 @@ class gkdtree:
         lats : Union[float, int, np.ndarray, list]
         depths : Union[float, int, np.ndarray, list, None], optional
         """
-        self.xyz = convert.lonlatdep2xyz(lons, lats, depths)
+        self.lons = lons
+        self.lats = lats
+        self.depths = depths
+        if convert2xyz:
+            self.xyz = convert.lonlatdep2xyz(lons, lats, depths)
     
     def add_vals(self,
                  vals: Union[float, int, np.ndarray, list]):
@@ -49,7 +57,8 @@ class gkdtree:
     def add_lonlatdep_query(self, 
                             lons: Union[float, int, np.ndarray, list], 
                             lats: Union[float, int, np.ndarray, list], 
-                            depths: Union[float, int, np.ndarray, list, None]=None):
+                            depths: Union[float, int, np.ndarray, list, None]=None,
+                            convert2xyz: bool=True):
         """Add lons/lats/depths of the queries
 
         Parameters
@@ -58,7 +67,11 @@ class gkdtree:
         lats : Union[float, int, np.ndarray, list]
         depths : Union[float, int, np.ndarray, list, None], optional
         """
-        self.xyz_q = convert.lonlatdep2xyz(lons, lats, depths)
+        self.lons_q = lons
+        self.lats_q = lats
+        self.depths_q = depths
+        if convert2xyz:
+            self.xyz_q = convert.lonlatdep2xyz(lons, lats, depths)
 
     def add_xyz(self, 
                 x: Union[list, np.ndarray], 
@@ -94,17 +107,20 @@ class gkdtree:
         z = convert.convert2array(z)
         self.xyz_q = np.vstack([x, y, z]).T
 
-    def create_tree(self):
+    def create_kdt(self):
         """Create a KD-tree based on self.xyz
         """
         if self.xyz is None:
-            print("[ERROR] xyz could not be found! Use add_lonlatdep.")
-            return None
+            try:
+                self.xyz = convert.lonlatdep2xyz(self.lons, self.lats, self.depths)
+            except Exception:
+                print("[ERROR] xyz could not be found! Use add_lonlatdep_query.")
+                return None
         self.kdt = spatial.cKDTree(self.xyz)
     
-    def query(self, 
-              num_neighs: int=4, 
-              distance_upper: Union[int, float]=np.inf):
+    def query_kdt(self, 
+                  num_neighs: int=4, 
+                  distance_upper: Union[int, float]=np.inf):
         """Query self.kdt (kd-tree)
 
         Parameters
@@ -112,18 +128,52 @@ class gkdtree:
         num_neighs : int, optional
         distance_upper : Union[int, float, np.inf], optional
         """
+        if self.xyz_q is None:
+            try:
+                self.xyz_q = convert.lonlatdep2xyz(self.lons_q, self.lats_q, self.depths_q)
+            except Exception:
+                print("[ERROR] Query's xyz_q could not be found! Use add_lonlatdep.")
+                return None
+
         if self.kdt is None:
             print("[WARNING] kdt could not be found.")
             print("[WARNING] create kd-tree...")
-            self.create_tree()
+            self.create_kdt()
             print("done")
+
         self.dists2query, self.indxs2query = \
             self.kdt.query(self.xyz_q, 
             k=num_neighs, 
             distance_upper_bound=distance_upper)
     
+    def query_balltree(self, 
+                       num_neighs: int=4):
+        """Query self.balltree 
+
+        Parameters
+        ----------
+        num_neighs : int, optional
+        distance_upper : Union[int, float, np.inf], optional
+        """
+
+        if self.balltree is None:
+            print("[WARNING] balltree could not be found.")
+            print("[WARNING] create balltree...")
+            self.create_balltree()
+            print("done")
+
+        self.dists2query, self.indxs2query = \
+            self.balltree.query(np.radians(np.vstack([self.lats_q, self.lons_q]).T), 
+                                k=num_neighs)
+        self.dists2query *= self.earth_radius
+    
+    def create_balltree(self):
+        self.balltree = BallTree(np.radians(np.vstack([self.lats, self.lons]).T), 
+                                 metric="haversine")
+    
     def interpolate(self, 
                     num_neighs: int=4, 
+                    method: str="kdtree",
                     diveps: float=1e-10):
         """Interpolate values of one grid into another one
 
@@ -134,7 +184,13 @@ class gkdtree:
         """
     
         # 1. dists/indices of neighbouring points
-        self.query(num_neighs)
+        self.interp_method = method
+        if method == "kdtree":
+            self.query_kdt(num_neighs)
+        elif method == "balltree":
+            self.query_balltree(num_neighs)
+        else:
+            print(f"method: {method} is not implemented!")
         if num_neighs == 1:
             self.dists2query = self.dists2query.reshape(-1, 1)
             self.indxs2query = self.indxs2query.reshape(-1, 1)
