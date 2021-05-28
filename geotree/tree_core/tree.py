5f44adb6ffccfac868c0bc87df9af5b3da421147
kasra-hosseini <khosseini@turing.ac.uk>
2021-05-28 14:30:35 +0100

    review tree.py

--- geotree/tree_core/tree.py
+++ geotree/tree_core/tree.py
@@ -14,13 +14,17 @@ from geotree.utils import convert
 class gtree:
     def __init__(self):
         # xyz: base
-        # kdt: kd-tree created from base
         # xyz_q: query
         self.xyz = None
         self.xyz_q = None
+        # lats/lons/depths
+        self.lats = None
+        self.lons = None
+        self.depths = None
+        # kdt: KDTree
         self.kdt = None
         self.balltree = None
-        self.earth_radius = 6371000
+        self.earth_radius_m = 6371000
 
     def add_lonlatdep(self, 
                       lons: Union[float, int, np.ndarray, list], 
@@ -34,6 +38,7 @@ class gtree:
         lons : Union[float, int, np.ndarray, list]
         lats : Union[float, int, np.ndarray, list]
         depths : Union[float, int, np.ndarray, list, None], optional
+        convert2xyz : bool, optional
         """
         self.lons = lons
         self.lats = lats
@@ -49,9 +54,6 @@ class gtree:
         ----------
         vals : Union[float, int, np.ndarray, list]
         """
-        if len(vals) != self.xyz.shape[0]:
-            print(f"[ERROR] vals should have {self.xyz.shape[0]} values")
-            return None
         self.vals = convert.convert2array(vals)
     
     def add_lonlatdep_query(self, 
@@ -66,6 +68,7 @@ class gtree:
         lons : Union[float, int, np.ndarray, list]
         lats : Union[float, int, np.ndarray, list]
         depths : Union[float, int, np.ndarray, list, None], optional
+        convert2xyz : bool, optional
         """
         self.lons_q = lons
         self.lats_q = lats
@@ -107,20 +110,20 @@ class gtree:
         z = convert.convert2array(z)
         self.xyz_q = np.vstack([x, y, z]).T
 
-    def create_kdt(self):
-        """Create a KD-tree based on self.xyz
-        """
+    def create_kdt(self, **kwds):
+        """Create a KD-tree based on self.xyz"""
         if self.xyz is None:
             try:
                 self.xyz = convert.lonlatdep2xyz(self.lons, self.lats, self.depths)
             except Exception:
-                print("[ERROR] xyz could not be found! Use add_lonlatdep_query.")
+                print("[ERROR] xyz could not be found! Use add_lonlatdep")
                 return None
-        self.kdt = spatial.cKDTree(self.xyz)
+        self.kdt = spatial.cKDTree(self.xyz, **kwds)
     
     def query_kdt(self, 
                   num_neighs: int=4, 
-                  distance_upper: Union[int, float]=np.inf):
+                  distance_upper: Union[int, float]=np.inf,
+                  **kwds):
         """Query self.kdt (kd-tree)
 
         Parameters
@@ -132,45 +135,51 @@ class gtree:
             try:
                 self.xyz_q = convert.lonlatdep2xyz(self.lons_q, self.lats_q, self.depths_q)
             except Exception:
-                print("[ERROR] Query's xyz_q could not be found! Use add_lonlatdep.")
+                print("[ERROR] Query's xyz_q could not be found! Use add_lonlatdep_query")
                 return None
 
         if self.kdt is None:
-            print("[WARNING] kdt could not be found.")
-            print("[WARNING] create kd-tree...")
+            print("[WARNING] kdt could not be found. Start creating kd-tree...")
             self.create_kdt()
             print("done")
 
         self.dists2query, self.indxs2query = \
             self.kdt.query(self.xyz_q, 
-            k=num_neighs, 
-            distance_upper_bound=distance_upper)
+                           k=num_neighs, 
+                           distance_upper_bound=distance_upper,
+                           **kwds)
     
+    def create_balltree(self, metric: str="haversine", **kwds):
+        """Create Ball tree based on self.lats and self.lons"""
+        if (self.lats is None) or (self.lons is None):
+            print(f"Either self.lats or self.lons is None! Exit.")
+            return None
+        self.balltree = BallTree(np.radians(np.vstack([self.lats, self.lons]).T), 
+                                 metric=metric, 
+                                 **kwds)
+
     def query_balltree(self, 
-                       num_neighs: int=4):
+                       num_neighs: int=4,
+                       **kwds):
         """Query self.balltree 
 
         Parameters
         ----------
         num_neighs : int, optional
-        distance_upper : Union[int, float, np.inf], optional
         """
 
         if self.balltree is None:
-            print("[WARNING] balltree could not be found.")
-            print("[WARNING] create balltree...")
+            print("[WARNING] balltree could not be found. Start creating ball tree...")
             self.create_balltree()
             print("done")
 
         self.dists2query, self.indxs2query = \
             self.balltree.query(np.radians(np.vstack([self.lats_q, self.lons_q]).T), 
-                                k=num_neighs)
-        self.dists2query *= self.earth_radius
-    
-    def create_balltree(self):
-        self.balltree = BallTree(np.radians(np.vstack([self.lats, self.lons]).T), 
-                                 metric="haversine")
-    
+                                k=num_neighs, 
+                                **kwds)
+        # Convert to meters
+        self.dists2query *= self.earth_radius_m
+        
     def interpolate(self, 
                     num_neighs: int=4, 
                     method: str="kdtree",
@@ -180,22 +189,27 @@ class gtree:
         Parameters
         ----------
         num_neighs : int, optional
+        method : str, optional
         diveps : float, optional
         """
     
-        # 1. dists/indices of neighbouring points
+        # 1. dists/indices of neighboring points
         self.interp_method = method
-        if method == "kdtree":
+
+        if method.lower() == "kdtree":
             self.query_kdt(num_neighs)
-        elif method == "balltree":
+        elif method.lower() == "balltree":
             self.query_balltree(num_neighs)
         else:
             print(f"method: {method} is not implemented!")
+            return None
+
+        # reshape dists2query and indxs2query when:
         if num_neighs == 1:
             self.dists2query = self.dists2query.reshape(-1, 1)
             self.indxs2query = self.indxs2query.reshape(-1, 1)
 
-        # 2. weights
+        # 2. weights (1 / distance)
         self.dists2query[self.dists2query < diveps] = diveps 
         weights = np.divide(1., self.dists2query)
 
