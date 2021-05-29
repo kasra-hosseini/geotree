125079f7185896e6bbfbedb6f781f414545102f8
kasra-hosseini <khosseini@turing.ac.uk>
2021-05-29 11:23:43 +0100

    add two new methods: lonlatdep2xyz_spherical and xyz2lonlatdep_spherical

--- geotree/utils/convert.py
+++ geotree/utils/convert.py
@@ -5,18 +5,74 @@ __author__ = "Kasra Hosseini"
 __license__ = "MIT License"
 
 import numpy as np
-import pyproj
 from typing import Union
 
-# Define projections
-ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
-lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+try:
+    import pyproj
+    pyproj_imported = True
+except ImportError:
+    pyproj_imported = False
+
+
+def lonlatdep2xyz_spherical(lons: Union[float, int, np.ndarray, list], 
+                            lats: Union[float, int, np.ndarray, list], 
+                            depths: Union[float, int, np.ndarray, list, None]=None,
+                            return_one_arr: Union[bool]=True, 
+                            earth_radius_m: Union[float, int]=6371000):
+    """Convert lons/lats/depths to x/y/z
+
+    Parameters
+    ----------
+    lons : Union[float, int, np.ndarray, list]
+    lats : Union[float, int, np.ndarray, list]
+    depths : Union[float, int, np.ndarray, list, None], optional
+    return_one_arr : Union[bool], optional
+    earth_radius_m : Union[float, int], optional
+    """
+    # co-latitudes:
+    colats = 90.0 - lats
+    # radii
+    r = earth_radius_m - depths
+
+    x = r * np.sin(np.deg2rad(colats)) * np.cos(np.deg2rad(lons))
+    y = r * np.sin(np.deg2rad(colats)) * np.sin(np.deg2rad(lons))
+    z = r * np.cos(np.deg2rad(colats))
+
+    if return_one_arr:
+        return np.vstack([x, y, z]).T 
+    else:
+        return x, y, z
+
+def xyz2lonlatdep_spherical(x: Union[float, int, np.ndarray, list], 
+                            y: Union[float, int, np.ndarray, list], 
+                            z: Union[float, int, np.ndarray, list, None]=None,
+                            return_one_arr: Union[bool]=True, 
+                            earth_radius_m: Union[float, int]=6371000):
+    """Convert x/y/z/ to lons/lats/depths
+
+    Parameters
+    ----------
+    x : Union[float, int, np.ndarray, list]
+    y : Union[float, int, np.ndarray, list]
+    z : Union[float, int, np.ndarray, list, None], optional
+    return_one_arr : Union[bool], optional
+    earth_radius_m : Union[float, int], optional
+    """
+    rxy = np.sqrt(x**2 + y**2)
+    lons = np.arctan2(y, x)*180./np.pi
+    lats = np.arctan2(z, rxy)*180./np.pi
+    depths = earth_radius_m - np.sqrt(x**2 + y**2 + z**2)
+
+    if return_one_arr:
+        return np.vstack([lons, lats, depths]).T 
+    else:
+        return lons, lats, depths
 
 def lonlatdep2xyz(lons: Union[float, int, np.ndarray, list], 
                   lats: Union[float, int, np.ndarray, list], 
                   depths: Union[float, int, np.ndarray, list, None]=None,
                   return_one_arr: Union[bool]=True):
-    """Convert lons/lats/depths to xyz
+    """Convert lons/lats/depths to x/y/z
 
     Parameters
     ----------
@@ -25,6 +81,15 @@ def lonlatdep2xyz(lons: Union[float, int, np.ndarray, list],
     depths : Union[float, int, np.ndarray, list, None], optional
     return_one_arr : Union[bool], optional
     """
+    
+    if not pyproj_imported:
+        print(f"[WARNING] pyproj could not be imported.")
+        print(f"[WARNING] lonlatdep2xyz_spherical will be used insted.")
+        lonlatdep2xyz_spherical(lons, lats, depths, return_one_arr)
+    
+    # Define projections
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
 
     lons = convert2array(lons)
     lats = convert2array(lats)
@@ -34,8 +99,9 @@ def lonlatdep2xyz(lons: Union[float, int, np.ndarray, list],
         depths = convert2array(depths)
 
     # trainsform to x, y, z (in meters)
+    # -1 * depth as pyproj.transform expects altitude
     x, y, z = pyproj.transform(lla, ecef,
-                               lons, lats, depths,
+                               lons, lats, -1*depths,
                                radians=False)
 
     if return_one_arr:
